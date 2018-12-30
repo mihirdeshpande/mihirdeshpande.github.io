5d2b1733753fbe0be4594b048c552914c3512964
mihirdeshpande <mihirdeshpande@users.noreply.github.com>
2018-12-30 18:46:56 -0500

    Update hello.js
    First working version

--- hello.js
+++ hello.js
@@ -48,19 +48,19 @@ class ThreeByThreeGrid extends React.Component{
 		return (
 			<div>
 				<div style={{width:"150"}}>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='0' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='1' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='2' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="0" propTest={this.props.propTest} gridId={this.props.gridId} squareId='0' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="1" propTest={this.props.propTest} gridId={this.props.gridId} squareId='1' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="2" propTest={this.props.propTest} gridId={this.props.gridId} squareId='2' sqColor={this.props.gridColor}/></div>
 				</div>
 				<div  style={{width:"150"}}>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='3' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='4' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='5' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="3" propTest={this.props.propTest} gridId={this.props.gridId} squareId='3' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="4" propTest={this.props.propTest} gridId={this.props.gridId} squareId='4' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="5" propTest={this.props.propTest} gridId={this.props.gridId} squareId='5' sqColor={this.props.gridColor}/></div>
 				</div>
 				<div  style={{width:"150"}}>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='6' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='7' sqColor={this.props.gridColor}/></div>
-					<div style={{display:"inline-block"}}><Square propTest={this.props.propTest} gridId={this.props.gridId} squareId='8' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="6" propTest={this.props.propTest} gridId={this.props.gridId} squareId='6' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="7" propTest={this.props.propTest} gridId={this.props.gridId} squareId='7' sqColor={this.props.gridColor}/></div>
+					<div style={{display:"inline-block"}}><Square ref="8" propTest={this.props.propTest} gridId={this.props.gridId} squareId='8' sqColor={this.props.gridColor}/></div>
 				</div>
 			</div>
 		);
@@ -82,37 +82,117 @@ class SudokuGrid extends React.Component{
 		 ['','','','','','','','',''],
 		 ['','','','','','','','','']
 		];
+		this.printToGUI = this.printToGUI.bind(this);
+	}
+	
+	printToGUI(e){
+		this.grid = this.solve();
+		for(var i=0;i<9;i++){
+			for(var j=0;j<9;j++){
+				var row = Math.floor(parseInt(i)/3)*3 + Math.floor(parseInt(j)/3);
+				var col = (parseInt(i)%3)*3 + (parseInt(j)%3);
+				this.refs[i].refs[j].setState({val:this.grid[row][col]});
+			}
+		}
+		
 	}
 	
 	test(num, threeByThreeGridNo, squareNo){
 		var row = Math.floor(parseInt(threeByThreeGridNo)/3)*3 + Math.floor(parseInt(squareNo)/3);
 		var col = (parseInt(threeByThreeGridNo)%3)*3 + (parseInt(squareNo)%3);
-		//console.log(threeByThreeGridNo);
-		//console.log(squareNo);
-		//console.log(row);
-		//console.log(col);
 		this.grid[row][col] = num;
-		//console.log(this.grid);
+	}
+	
+	getNextCell(grid, row, col){
+		for(var i = row; i < 9; i++){
+			for(var j = 0; j < 9; j++){
+				if (grid[i][j] === ''){
+					return [i, j];
+				}
+				
+			}
+		}
+		return [null, null];
+	}
+	
+	isValid(grid, row, col, val){
+		// check in row
+		for(var i=0; i<9;i++){
+			if (grid[row][i] === val){
+				return false;
+			}
+		}
+		
+		// check in col
+		for(var i=0; i<9;i++){
+			if (grid[i][col] === val){
+				return false;
+			}
+		}
+		
+		// check in 3*3 grid
+		var rowStart = Math.floor(parseInt(row)/3)*3;
+		var colStart = Math.floor(parseInt(col)/3)*3;
+		for(var i=0; i<3;i++){
+			for(var j=0; j<3;j++){
+				if (grid[rowStart+i][colStart+j] === val){
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	solveSudoku(grid, row, col){
+		// Find next unassigned cell
+		var cell = this.getNextCell(grid, row, col);
+		console.log(cell);
+		if (cell[0] == null){
+			return true;
+		}
+		for (var i=1; i<=9;i++){
+			if (this.isValid(grid, cell[0], cell[1], i.toString())){
+				grid[cell[0]][cell[1]] = i.toString();
+				if (this.solveSudoku(grid, cell[0], cell[1])){
+					return true;
+				}
+				grid[cell[0]][cell[1]] = '';
+			}
+		}
+		
+		return false;
+	}
+	
+	solve(){
+		var gridCopy = [];
+		for (var i = 0; i < this.grid.length; i++){
+			gridCopy[i] = this.grid[i].slice();
+		}
+		var result = this.solveSudoku(gridCopy, 0, 0);
+		console.log(result);
+		return gridCopy;
 	}
 	
 	render(){
 		return (
-			<div>
+			<div ref="sudoku">
 				<div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='0' gridColor="rgba(150, 245, 120, 0.8)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='1' gridColor="rgba(102, 245, 79, 0.9)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='2' gridColor="rgba(150, 245, 120, 0.8)"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='0' gridColor="rgba(150, 245, 120, 0.8)" ref="0"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='1' gridColor="rgba(102, 245, 79, 0.9)" ref="1"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='2' gridColor="rgba(150, 245, 120, 0.8)" ref="2"/></div>
 				</div>
 				<div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='3' gridColor="rgba(102, 245, 79, 0.9)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='4' gridColor="rgba(150, 245, 120, 0.8)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='5' gridColor="rgba(102, 245, 79, 0.9)"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='3' gridColor="rgba(102, 245, 79, 0.9)" ref="3"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='4' gridColor="rgba(150, 245, 120, 0.8)" ref="4"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='5' gridColor="rgba(102, 245, 79, 0.9)" ref="5"/></div>
 				</div>
 				<div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='6' gridColor="rgba(150, 245, 120, 0.8)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='7' gridColor="rgba(102, 245, 79, 0.9)"/></div>
-					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='8' gridColor="rgba(150, 245, 120, 0.8)"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='6' gridColor="rgba(150, 245, 120, 0.8)" ref="6"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='7' gridColor="rgba(102, 245, 79, 0.9)" ref="7"/></div>
+					<div style={{display:"inline-block"}}><ThreeByThreeGrid propTest={this.test} gridId='8' gridColor="rgba(150, 245, 120, 0.8)" ref="8"/></div>
 				</div>
+				<button className="btn-lg" onClick={(e) => this.printToGUI(e)}>Solve</button>
 			</div>
 		);
 	}
